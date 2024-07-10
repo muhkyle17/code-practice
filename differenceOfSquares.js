b8ac274105e4c3b5902b154ecbef8d7420a7c18c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-10 05:57:30 -0700

    Add a nicer version of same code

--- differenceOfSquares.js
+++ differenceOfSquares.js
@@ -14,3 +14,17 @@ function differenceOfSquares(n) {
 
 const number = differenceOfSquares(10)
 console.log(number, 'number')
+
+function differenceOfSquares2(n) {
+  let array = []
+  for (let i = n; i >= 1; i--) {
+    array.push(i)
+  }
+  const squareOfSum = array.reduce((acc, curr) => acc + curr, 0) ** 2
+  const sumOfSqaures = array.map(num => num ** 2).reduce((acc, curr) => acc + curr, 0)
+
+  return squareOfSum - sumOfSqaures
+}
+
+const number2 = differenceOfSquares2(10)
+console.log(number2, 'number2')
