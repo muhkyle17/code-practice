94857ed0e049fb5aad44ee4abc5fd950cdb2193c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-15 23:59:53 -0700

    Add working solution

--- numberPairs.js
+++ numberPairs.js
@@ -46,3 +46,21 @@ let arr2 = [84, 78, -28, 0, -37]
 
 const newArray = getLargerNumbers(arr1, arr2) // Returns [23, 64, 53, 17, 88]
 console.log(newArray, 'newArray')
+
+function getLargerNumbers(a, b) {
+  let array = [...a]
+  for (let x = 0; x < array.length; x++) {
+    if (a[x] < b[x]) {
+      array[x] = b[x]
+    }
+  }
+  return array
+}
+
+// let arr1 = [13, 64, 15, 17, 88];
+// let arr2 = [23, 14, 53, 17, 80];
+let arr3 = [-91, -64, -56, 90, -91]
+let arr4 = [84, 78, -28, 0, -37]
+
+const newArray2 = getLargerNumbers2(arr1, arr2) // Returns [23, 64, 53, 17, 88]
+console.log(newArray, 'newArray')
