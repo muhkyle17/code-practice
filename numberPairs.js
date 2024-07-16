2543a4f27c97281d961c0385c53c8629599129e1
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-15 18:17:40 -0700

    This logic works but doesn't account for when number is 0

--- numberPairs.js
+++ numberPairs.js
@@ -24,3 +24,25 @@
 
 // const newArray = getLargerNumbers(arr1, arr2) // Returns [23, 64, 53, 17, 88]
 // console.log(newArray, 'newArray')
+
+function getLargerNumbers(a, b) {
+  let array = [...a]
+  for (let x = 0; x < array.length; x++) {
+    if (a[x] >= b[x]) {
+      continue
+    } else if (a[x] < b[x]) {
+      const thisArray = array.filter(item => item !== a[x])
+      thisArray.splice(x, 0, b[x])
+      array = thisArray
+    }
+  }
+  return array
+}
+
+// let arr1 = [13, 64, 15, 17, 88];
+// let arr2 = [23, 14, 53, 17, 80];
+let arr1 = [-91, -64, -56, 90, -91]
+let arr2 = [84, 78, -28, 0, -37]
+
+const newArray = getLargerNumbers(arr1, arr2) // Returns [23, 64, 53, 17, 88]
+console.log(newArray, 'newArray')
