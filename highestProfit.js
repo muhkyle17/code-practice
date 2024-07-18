2481561c189dc23fb85c0e545747ac86b743195b
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-18 11:34:24 -0700

    Return optimal solution that I know to do not the previous solution

--- highestProfit.js
+++ highestProfit.js
@@ -1,8 +1,4 @@
-function minMax(arr) {
-  const max = Math.max(...arr)
-
-  return [Math.min(...arr), Math.max(...arr)]
-}
+const minMax = arr => [Math.min(...arr), Math.max(...arr)]
 
 const profit = minMax([1, 2, 3, 4, 5])
 console.log(profit, 'profit')
