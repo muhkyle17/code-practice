a7852d4c4c26363cf2bab96d62d7056bdb12f8a9
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-20 12:37:25 -0700

    Add a solution with for loop

--- reversedString.js
+++ reversedString.js
@@ -6,3 +6,17 @@ String.prototype.reverse = function () {
 // Example usage:
 const reversed = 'Super awesome string'.reverse()
 console.log(reversed, 'reversed') // => "gnirts emosewa repuS"
+
+// Add reverse method to String prototype
+String.prototype.reverse2 = function () {
+  const array = [...this]
+  let reversedArray = []
+  for (let i = array.length; i >= 0; i--) {
+    reversedArray.push(array[i])
+  }
+  return reversedArray.join('')
+}
+
+// Example usage:
+const reversed2 = 'Super awesome string'.reverse2()
+console.log(reversed2, 'reversed2') // => "gnirts emosewa repuS"
