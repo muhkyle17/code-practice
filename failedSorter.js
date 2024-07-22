87d97b61212e2a90a8f1661f6b436412555bdfd0
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-22 10:53:05 -0700

    Make it easier to understand

--- failedSorter.js
+++ failedSorter.js
@@ -1,10 +1,21 @@
 Array.prototype.sort = function () {
-  for (let i = 0; i < this.length - 1; i++)
-    for (let j = i + 1; j < this.length; j++)
-      this[i] > this[j] && ([this[i], this[j]] = [this[j], this[i]])
+  for (let i = 0; i < this.length - 1; i++) {
+    for (let j = i + 1; j < this.length; j++) {
+      // Log the current elements being compared
+      console.log(`Comparing this[${i}] = ${this[i]} and this[${j}] = ${this[j]}`)
 
+      if (this[i] > this[j]) {
+        // Swap the elements if they are in the wrong order
+        ;[this[i], this[j]] = [this[j], this[i]]
+
+        // Log the elements after swapping
+        console.log(`Swapped: this[${i}] = ${this[i]} and this[${j}] = ${this[j]}`)
+      }
+    }
+  }
   return this
 }
 
+// Example usage
 const sorted = [9, 7, 2, 4, 5, 3, 6, 8, 1].sort()
 console.log(sorted, 'sorted')
